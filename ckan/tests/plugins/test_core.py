affd2c414bb73eebc8e35a090268b5f8026de33f
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2021-12-22 13:27:33 +0200

    Different fix for the same test

--- ckan/tests/plugins/test_core.py
+++ ckan/tests/plugins/test_core.py
@@ -127,9 +127,9 @@ def reset_observer():
     plugins.unload("test_observer_plugin")
 
 
+@pytest.mark.ckan_config("ckan.plugins", "mapper_plugin action_plugin")
+@pytest.mark.usefixtures("with_plugins")
 def test_plugins_load(monkeypatch):
-    monkeypatch.setitem(config, "ckan.plugins", "mapper_plugin action_plugin")
-    plugins.load_all()
     # synchronous_search automatically gets loaded
     current_plugins = set(
         [
@@ -186,8 +186,6 @@ def test_mapper_plugin_fired_on_delete():
     ]
 
 
-@pytest.mark.ckan_config("ckan.plugins", "")
-@pytest.mark.usefixtures("with_plugins")
 def test_action_plugin_override():
     status_show_original = logic.get_action("status_show")(None, {})
     with plugins.use_plugin("action_plugin"):
